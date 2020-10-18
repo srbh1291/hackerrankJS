580f56024331b4e3d89c4fe8ccf6a68bb414d3d3
Saurabh Pande <srbh_91@outlook.com>
2020-10-18 19:57:58 +0530

    Update getNextPrimeNo.js

--- getNextPrimeNo.js
+++ getNextPrimeNo.js
@@ -1,17 +1,9 @@
-// 2,3,5,7,11 ....... 6
-
 const generatePrimeNos = ((num) => {
       num++;
       for (let i = 2; i < num; i++) {
          if(num%i === 0) {
             num++;
-            i=2;
-         } else {
-            continue;
          }
       }
       return num;
-})(5)
-
-
-console.log(generatePrimeNos)
+})(1797)
